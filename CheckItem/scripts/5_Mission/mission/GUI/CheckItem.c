6f71633eba9747c183bcbcc2c170fab24beeb2e7
micromartin1963 <micromartin1963@gmail.com>
2023-01-09 08:23:10 +0000

    Add files via upload

--- CheckItem/scripts/5_Mission/mission/GUI/CheckItem.c
+++ CheckItem/scripts/5_Mission/mission/GUI/CheckItem.c
@@ -4,14 +4,22 @@ class CheckItem extends UIScriptedMenu
   	private bool m_IsMenuOpen;
 	int builderitems = 0;
 	int CodeLocks = 0;
+	int range = 15;
    	MultilineTextWidget m_InfoBox;
   	ButtonWidget m_Btnenable;
 	ButtonWidget m_Btndisable;
 	ButtonWidget m_BtnCancel;
 	protected TextWidget m_TextCenter;
-	protected TextWidget m_TextMoreInfo;	
-	bool m_active = false;
+	protected TextWidget m_TextMoreInfo;
+	protected TextWidget m_Range01;
+	protected TextWidget m_Range02;
+	protected TextWidget m_Range03;	
+	protected CheckBoxWidget m_CheckBox15mtr;
+	protected CheckBoxWidget m_CheckBox25mtr;
+	protected CheckBoxWidget m_CheckBox35mtr;
 
+	bool m_active = false;
+	bool uncheck = false;
 
 	void CheckItem()
 	{		
@@ -37,6 +45,9 @@ class CheckItem extends UIScriptedMenu
 	m_Btndisable = ButtonWidget.Cast( layoutRoot.FindAnyWidget( "btn_disable" ) );
 	m_BtnCancel = ButtonWidget.Cast( layoutRoot.FindAnyWidget( "btn_cancel" ) );
 	m_TextMoreInfo = TextWidget.Cast(layoutRoot.FindAnyWidget("MoreInfo"));
+	m_CheckBox15mtr = CheckBoxWidget.Cast(layoutRoot.FindAnyWidget("CheckBox15mtr"));
+	m_CheckBox25mtr = CheckBoxWidget.Cast(layoutRoot.FindAnyWidget("CheckBox25mtr"));
+	m_CheckBox35mtr = CheckBoxWidget.Cast(layoutRoot.FindAnyWidget("CheckBox35mtr"));
 	m_active = true;		
         return layoutRoot;
     }
@@ -63,6 +74,8 @@ class CheckItem extends UIScriptedMenu
 		//PPEffects.SetBlurMenu(0.5);
 		GetGame().GetInput().ChangeGameFocus(1);
 		SetFocus( layoutRoot );
+		m_CheckBox15mtr.SetChecked(true);
+		m_CheckBox15mtr.Enable(false);
 	}
 
 	override void OnHide()
@@ -85,10 +98,36 @@ class CheckItem extends UIScriptedMenu
 		m_TextMoreInfo.SetText("Building items : " + builderitems.ToString() );
 		}
 		
-		if ( w == m_Btndisable )
+		if ( w == m_CheckBox15mtr )
 		{
-		m_TextCenter.SetText("disable");
-			Print("Pressed disable");
+		range = 15;
+		m_CheckBox15mtr.Enable(false);
+		m_CheckBox25mtr.Enable(true);
+		m_CheckBox35mtr.Enable(true);
+		m_CheckBox25mtr.SetChecked(false);
+		m_CheckBox35mtr.SetChecked(false);
+			return true;
+		}
+
+		if ( w == m_CheckBox25mtr )
+		{
+		range = 25;
+		m_CheckBox25mtr.Enable(false);
+		m_CheckBox15mtr.Enable(true);
+		m_CheckBox35mtr.Enable(true);
+		m_CheckBox15mtr.SetChecked(false);
+		m_CheckBox35mtr.SetChecked(false);
+			return true;
+		}
+
+		if ( w == m_CheckBox35mtr )
+		{
+		range = 35;
+		m_CheckBox35mtr.Enable(false);
+		m_CheckBox25mtr.Enable(true);
+		m_CheckBox15mtr.Enable(true);
+		m_CheckBox25mtr.SetChecked(false);
+		m_CheckBox15mtr.SetChecked(false);
 			return true;
 		}
 		
@@ -119,31 +158,22 @@ class CheckItem extends UIScriptedMenu
 	{
 	int items = 0;
 	int itemsTotal = 0;
-	//string tmp ;
-        //Building building;
+	int v_items = 0;
+	int v_itemsTotal = 0;
 	ItemBase m_items;
-	//Inventory_Base IvItems;
-	//Container_Base ContainerItems;
-	//CargoBase cargo; 
-	//EntityAI Ent_Ai;
-
-
-
+	builderitems = 0;
 
 		ref array<Object> objects = new array<Object>;
 
-		GetGame().GetObjectsAtPosition3D(GetGame().GetPlayer().GetPosition(), 15, objects, NULL);
+		GetGame().GetObjectsAtPosition3D(GetGame().GetPlayer().GetPosition(), range, objects, NULL);
 		for (int i = 0; i < objects.Count(); i++) 
 		{
-
-
 			Class.CastTo(m_items, objects.Get(i));
 			if(m_items)
 			{
 			items = m_items.ListAllMyItems(m_items);
 			itemsTotal = itemsTotal + items;
 			//items++;
-
 			if(m_items.ToString().Contains("wall") )builderitems ++;
 			if(m_items.ToString().Contains("Wall") )builderitems ++;
 			if(m_items.ToString().Contains("floor") )builderitems ++;
@@ -156,20 +186,36 @@ class CheckItem extends UIScriptedMenu
 			if(m_items.ToString().Contains("Flag") )builderitems ++;
 			if(m_items.ToString().Contains("lock") )builderitems ++;
 			if(m_items.ToString().Contains("CodeLock") )CodeLocks ++;
-
 			}
-		
+		}
 
+		return itemsTotal - builderitems - CodeLocks;
+		
+	}
+
+
+
+	int Count_V_Items() 
+	{
+	int vitemsTotal = 0;
+	CargoBase myCargo;
+
+		ref array<Object> objects = new array<Object>;
+
+		GetGame().GetObjectsAtPosition3D(GetGame().GetPlayer().GetPosition(), range, objects, NULL);
+		for (int i = 0; i < objects.Count(); i++) 
+		{
+			Class.CastTo(myCargo, objects.Get(i));
+			if(myCargo)
+			{
+			vitemsTotal = myCargo.GetItemCount();
+			}
 
 
 		}
 
-
-		return itemsTotal - builderitems - CodeLocks;
-
-		
+		return vitemsTotal ;		
 	}
-
 
 
 
